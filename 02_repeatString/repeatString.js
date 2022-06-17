da241a5901570736e11a4c70df2e46ed9cefad7d
yohannesnan <yohannessetegn18@gmail.com>
2022-06-17 19:49:46 +0300

    passed the test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,23 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let storage = ""// creating a string variable 
+    if (num < 0){
+        storage = "ERROR";
+    }
+    else{
+        
+        for(let i = 0; i<num; i++){
+            storage += string;
+        }
+    }
+    return(storage);
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+
+
+
+
+
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,39 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let arrayTemp = string.split(" ");//converts string to array when finds a space
+    for(let i = 0; i<arrayTemp.length; i++){
+        arrayTemp[i]=sortingReversely(arrayTemp[i]);
+    }
+    sortTheArray(arrayTemp);
+    return arrayTemp.join(" ");
+  };
+  
+  
+  function sortingReversely(a){
+    return (a.split('').reverse().join(''));//reverses the string 
+  }
+  
+  function sortTheArray(a){
+    if (a.length % 2 == 0){
+      let last = a.length-1;
+      for(let i=0; i< a.length/2; i++, last--){
+        let temp = a[i];
+        a[i] = a[last];
+        a[last]  = temp;
+      }
+    
+    }
+    else{
+      let last = a.length-1;
+      for(let i=0; i< (a.length - 1)/2; i++, last--){
+        let temp = a[i];
+        a[i] = a[last];
+        a[last]  = temp;
+    }
+    }
+    return a;
+  }
+  console.log(reverseString('hello there'));
+  
+  
 // Do not edit below this line
 module.exports = reverseString;

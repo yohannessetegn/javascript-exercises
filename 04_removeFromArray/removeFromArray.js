ec85592b15045a79fba08ee7b19ba3f3cb39ae15
yohannesnan <yohannessetegn18@gmail.com>
2022-06-20 15:13:37 +0300

    modified:   04_removeFromArray/removeFromArray.js
    solved the problem

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(newArray, otherThing) {
+    for(let i=0; i < newArray.length; i++){
+        let ok;
+        if(newArray[i] === otherThing){
+            ok = newArray.splice(i,1);
+        }
+        else{
 
+        }
+        return ok;
+    }
 };
 
 // Do not edit below this line

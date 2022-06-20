ed167e7d17788abb7d4a092d13092594fb5641ea
yohannesnan <yohannessetegn18@gmail.com>
2022-06-20 16:36:30 +0300

    solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(args) {
+  let celsius, finalCelsius;
+  celsius = (args - 32) * (5/9);
+  finalCelsius = Number(celsius.toFixed(1))
+  return(finalCelsius);
 };
 
-const ctof = function() {
-
+const ctof = function(args) {
+  let Fahrenheit, finalFahrenheit;
+  Fahrenheit = (args * (9/5)) + 32;
+  finalFahrenheit = Number(Fahrenheit.toFixed(1))
+  return(finalFahrenheit);
 };
 
 // Do not edit below this line

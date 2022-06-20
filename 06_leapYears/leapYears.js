27b9e2372e57c47c614c707f78f017db0dd25738
yohannesnan <yohannessetegn18@gmail.com>
2022-06-20 16:12:44 +0300

    solved

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function(args) {
+ if(args%4 === 0 && args%100 != 0){
+    return true;
+ }
+else if(args%100 === 0 && args%400 === 0){
+        return true;
+}
+else{return false;}
 
-};
+ }
+
 
 // Do not edit below this line
 module.exports = leapYears;
